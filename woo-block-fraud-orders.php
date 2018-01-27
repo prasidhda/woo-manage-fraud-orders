9cf930665ebea637c8c4e82a6c989ecf81ecd181
Prasidhda Malla <pranuk.prwnsi@gmail.com>
2018-01-27 17:15:17 +0545

    Plugin description added.

---
+++ woo-block-fraud-orders.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  Woo Block Fraud Orders
+Plugin URI:   https://github.com/prasidhda/woo-block-fraud-orders
+Description:  WooCommerce plugin to block the fraud orders. 
+Version:      0.1
+Author:       Prasidhds Malla
+Author URI:   https://profiles.wordpress.org/prasidhda
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  woo-block-fraud-orders
+Domain Path:  /languages
+*/
