a6376cb3918822f2d3c71a5f16cf4af2fae6acc3
Prasidhda Malla <pranuk.prwnsi@gmail.com>
2021-07-14 23:28:27 +0545

    fix: multisite plugin activation

--- includes/class-woo-manage-fraud-orders.php
+++ includes/class-woo-manage-fraud-orders.php
@@ -92,7 +92,26 @@ if ( ! class_exists( 'Woo_Manage_Fraud_Orders' ) ) {
 		 * Check is WooCommerce active.
 		 */
 		public function install() {
-			if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ), true ) ) {
+
+			if ( ! function_exists( 'is_plugin_active_for_network' ) ) {
+				require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
+			}
+
+			// multisite
+			if ( is_multisite() ) {
+				// this plugin is network activated - Woo must be network activated
+				if ( is_plugin_active_for_network( plugin_basename(__FILE__) ) ) {
+					$need = ! is_plugin_active_for_network( 'woocommerce/woocommerce.php' );
+					// this plugin is locally activated - Woo can be network or locally activated
+				} else {
+					$need = ! is_plugin_active( 'woocommerce/woocommerce.php' );
+				}
+				// this plugin runs on a single site
+			} else {
+				$need =  ! is_plugin_active( 'woocommerce/woocommerce.php');
+			}
+
+			if ( $need ) {
 
 				echo sprintf( esc_html__( 'Woo Manage Fraud Orders depends on %s to work!', 'woo-manage-fraud-orders' ), '<a href="https://wordpress.org/plugins/woocommerce/" target="_blank">' . esc_html__( 'WooCommerce', 'woo-manage-fraud-orders' ) . '</a>' );
 				@trigger_error( '', E_USER_ERROR );
