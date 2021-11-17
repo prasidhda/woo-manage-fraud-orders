4afc6c5990f5c0101ec1e671d09e8515ed594b32
Prasidhda Malla <pranuk.prwnsi@gmail.com>
2021-11-17 16:25:32 +0545

    versioning 2.5.1

--- includes/class-woo-manage-fraud-orders.php
+++ includes/class-woo-manage-fraud-orders.php
@@ -23,7 +23,7 @@ if ( ! class_exists( 'Woo_Manage_Fraud_Orders' ) ) {
 		 *
 		 * @var string $version
 		 */
-		public $version = '2.5.0';
+		public $version = '2.5.1';
 
 		/**
 		 * Store the class singleton.
